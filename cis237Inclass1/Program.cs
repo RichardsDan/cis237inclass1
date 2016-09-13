371fb12e7f903ae49c3ed402980a767431413d98
Daniel Richards <d.j.richards@comcast.net>
2016-09-13 16:52:21 -0400

    Ass Employee class and some code to main

--- cis237Inclass1/Program.cs
+++ cis237Inclass1/Program.cs
@@ -10,6 +10,29 @@ namespace cis237Inclass1
     {
         static void Main(string[] args)
         {
+            Employee myEmployee = new Employee();
+            myEmployee.FirstName = "Daniel";
+            myEmployee.LastName = "Richards";
+            myEmployee.WeeklySalary = 2048.34m;
+
+            Console.WriteLine(myEmployee.FirstName);
+            Console.WriteLine(myEmployee);
+
+            Employee[] employees = new Employee[10];
+
+            employees[0] = new Employee("Frank", "Fontaine", 53.00m);
+            employees[1] = new Employee("Corvo", "Attano", 1280.00m);
+            employees[2] = new Employee("Emily", "Kaldwin", 3840.00m);
+            employees[3] = new Employee("Booker", "Dewitt", 453.00m);
+            employees[4] = new Employee("Andrew", "Ryan", 4633.00m);
+
+            foreach (Employee employee in employees)
+            {
+                if (employee != null)
+                {
+                    Console.WriteLine(employee.ToString() + " " + employee.YearlySalary());
+                }
+            }
         }
     }
 }
